10392d42713d73f17bd6bb759b088daa4876c545
victorgse <victorgse@gmail.com>
2014-06-07 20:25:52 +0100

    Updates to carousel (swipe and keyboard support)

--- static/js/victorgse-jquery.js
+++ static/js/victorgse-jquery.js
@@ -3,8 +3,25 @@ $(document).ready(function() {
     var controller = new ScrollMagic();
 
 	// build scene
-	var scene = new ScrollScene({triggerElement: "#trigger", duration: 500})
-		.setTween(TweenMax.to("#animate", 0.5, {scale: 2.5, color: "green"}))
+	var scene1 = new ScrollScene({triggerElement: "#animate", duration: 75})
+		.setTween(TweenMax.from("#animate", 1, {autoAlpha: 0}))
 		.addTo(controller);
 
+    $("#myCarousel").swiperight(function() {
+        $(this).carousel('prev');
+    });
+
+    $("#myCarousel").swipeleft(function() {
+        $(this).carousel('next');
+    });
+
+});
+
+$(document).bind('keyup', function(e) {
+    if(e.which == 39) {
+        $('.carousel').carousel('next');
+    }
+    else if(e.which == 37) {
+        $('.carousel').carousel('prev');
+    }
 });

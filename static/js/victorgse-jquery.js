dc59ab49c7de76322ba279046c9b03fad293b97f
victorgse <victorgse@gmail.com>
2014-06-09 17:42:16 +0100

    Added second parallax

--- static/js/victorgse-jquery.js
+++ static/js/victorgse-jquery.js
@@ -13,4 +13,15 @@ $(document).ready(function() {
             TweenMax.to("#parallax #parallaxbg", 1, {backgroundPosition: "0 -320%", ease: Linear.easeNone})
         ]));
 
+    new ScrollScene({
+            triggerElement: "section#parallax2",
+            duration: $(window).height() + 300,
+            offset: -217
+        })
+        .addTo(controller)
+        .triggerHook("onCenter")
+        .setTween(new TimelineMax().add([
+            TweenMax.to("#parallax2 #parallaxbg2", 1, {backgroundPosition: "0 -320%", ease: Linear.easeNone})
+        ]));
+
 });

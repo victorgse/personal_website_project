a903ab041445f79537492b29d281fc2ee66bc10d
victorgse <victorgse@gmail.com>
2014-06-09 10:48:06 +0100

    Work on parallax responsiveness

--- static/js/victorgse-jquery.js
+++ static/js/victorgse-jquery.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     new ScrollScene({
             triggerElement: "section#parallax",
             duration: $(window).height() + 300,
-            offset: -210
+            offset: -220
         })
         .addTo(controller)
         .triggerHook("onCenter")
         .setTween(new TimelineMax().add([
-            TweenMax.to("#parallax #parallaxbg", 1, {backgroundPosition: "0 -230%", ease: Linear.easeNone})
+            TweenMax.to("#parallax #parallaxbg", 1, {backgroundPosition: "0 -320%", ease: Linear.easeNone})
         ]));
 
 });

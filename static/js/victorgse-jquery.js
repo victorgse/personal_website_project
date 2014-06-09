fd6efe9678a994af0c1308e177243f73a8ac28e1
victorgse <victorgse@gmail.com>
2014-06-09 09:42:07 +0100

    Added parallax effect

--- static/js/victorgse-jquery.js
+++ static/js/victorgse-jquery.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
 
     var controller = new ScrollMagic();
 
-	// build scene
-	var scene1 = new ScrollScene({triggerElement: "#animate", duration: 75})
-	    .setTween(TweenMax.from("#animate", 1, {autoAlpha: 0}))
-	    .addTo(controller);
+    new ScrollScene({
+            triggerElement: "section#parallax",
+            duration: $(window).height() + 300,
+            offset: -210
+        })
+        .addTo(controller)
+        .triggerHook("onCenter")
+        .setTween(new TimelineMax().add([
+            TweenMax.to("#parallax #parallaxbg", 1, {backgroundPosition: "0 -230%", ease: Linear.easeNone})
+        ]));
 
 });
